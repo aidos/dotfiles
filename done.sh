f70e61be06b5c6cdf67851e48b2f39a88c5b38f1
Aidan Kane <aidankane@gmail.com>
2013-11-13 20:28:50 +0000

    minor tweaks (ps - this file is a hodgepodge of info)

--- done.sh
+++ done.sh
@@ -15,18 +15,24 @@ export HISTFILESIZE=1000000000
 export WORKON_HOME=/usr/local/virtualenvs
 source /usr/local/bin/virtualenvwrapper.sh
 " >> .bashrc
+
+export LANGUAGE=en_GB.UTF-8
+export LANG=en_GB.UTF-8
+export LC_ALL=en_GB.UTF-8
 sudo locale-gen en_GB.UTF-8
 sudo dpkg-reconfigure locales
 
 git clone git@github.com:aidos/dotfiles.git .dotfiles
+cd .dotfile
+git submodule init
+git submodule update
 ~/.dotfiles/link.sh 
 
 sudo easy_install virtualenv
 sudo easy_install pip
 sudo pip install virtualenvwrapper
 
-sudo mkdir /usr/local/virtualenvs
-export WORKON_HOME=/usr/local/virtualenvs
+#sudo mkdir /usr/local/virtualenvs
 export WORKON_HOME=~/.virtualenvs
 source /usr/local/bin/virtualenvwrapper.sh
 

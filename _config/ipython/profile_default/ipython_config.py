6d7a0fcaccfad95e50a93bb9136b6a6ce3ffd2a0
Aidan Kane <aidankane@gmail.com>
2017-02-15 09:48:24 +0000

    general updates

--- _config/ipython/profile_default/ipython_config.py
+++ _config/ipython/profile_default/ipython_config.py
@@ -5,3 +5,24 @@ c.InteractiveShellApp.exec_lines = ['%autoreload 2']
 c.InteractiveShellApp.extensions = ['autoreload']
 
 c.TerminalInteractiveShell.autoindent = False
+
+c.TerminalInteractiveShell.editing_mode = 'vi'
+
+c.InteractiveShell.colors='linux'
+
+#c.Completer.greedy = True
+
+
+## https://github.com/ipython/ipython/issues/9915
+# from IPython.terminal.prompts import Prompts, Token
+# from prompt_toolkit.key_binding.vi_state import InputMode
+# class MyPrompts(Prompts):
+#     def in_prompt_tokens(self, cli=None):
+#         mode = 'ins' if cli.vi_state.input_mode == InputMode.INSERT else 'cmd'
+#         return [
+#             (Token.Prompt, '(%s)In [' % mode),
+#             (Token.PromptNum, str(self.shell.execution_count)),
+#             (Token.Prompt, ']: ')
+#         ]
+# c.TerminalInteractiveShell.prompts_class = MyPrompts
+

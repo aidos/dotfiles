8e63a02c768bb5e1a5ea5dce98d81e8e4bbbcc82
Aidan Kane <aidankane@gmail.com>
2017-02-17 10:17:22 +0000

    fix tmux detach and ssh

--- link.sh
+++ link.sh
@@ -3,7 +3,10 @@
 ln -s ~/.dotfiles/_config/ipython ~/.ipython
 mkdir -p ~/.config
 ln -s ~/.dotfiles/_config/inkscape ~/.config/inkscape
+mkdir -p ~/.ssh
+ln -s ~/.dotfiles/_ssh/rc ~/.ssh/rc
 
 ln -s ~/.dotfiles/_ackrc ~/.ackrc
 ln -s ~/.dotfiles/_gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/_inputrc ~/.inputrc
+ln -s ~/.dotfiles/_tmux.conf ~/.tmux.conf

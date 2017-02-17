5dfaaedcc949a9be33c7e7606f4125a3cd399e54
Aidan Kane <aidankane@gmail.com>
2017-02-17 10:13:41 +0000

    basic tmux config

--- link.sh
+++ link.sh
@@ -7,3 +7,4 @@ ln -s ~/.dotfiles/_config/inkscape ~/.config/inkscape
 ln -s ~/.dotfiles/_ackrc ~/.ackrc
 ln -s ~/.dotfiles/_gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/_inputrc ~/.inputrc
+ln -s ~/.dotfiles/_tmux.conf ~/.tmux.conf

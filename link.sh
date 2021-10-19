a917a01fa26f9e1dbf740f1f811837931b343274
Aidan Kane <aidan@countfire.com>
2021-10-19 21:53:41 +0000

    better default config + neovim instructions

--- link.sh
+++ link.sh
@@ -2,10 +2,9 @@
 
 ln -s ~/.dotfiles/_config/ipython ~/.ipython
 mkdir -p ~/.config
-mkdir -p ~/.ssh
-ln -s ~/.dotfiles/_ssh/rc ~/.ssh/rc
+mkdir -p ~/.ssh && ln -s ~/.dotfiles/_ssh/rc ~/.ssh/rc
 
 ln -s ~/.dotfiles/_ackrc ~/.ackrc
-ln -s ~/.dotfiles/_gitconfig ~/.gitconfig
+cp ~/.dotfiles/_gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/_inputrc ~/.inputrc
 ln -s ~/.dotfiles/_tmux.conf ~/.tmux.conf

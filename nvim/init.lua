562ad62a2ddcd4e7100e77c215379bbcf4fe9947
Aidan Kane <aidan@countfire.com>
2022-01-12 21:47:15 +0000

    disable TS, syntax etc for big files

--- nvim/init.lua
+++ nvim/init.lua
@@ -20,5 +20,28 @@ vim.cmd([[
     autocmd!
     autocmd BufWritePost plugins.lua source <afile> | PackerCompile
   augroup end
+
+  " disable syntax highlighting in big files
+  function DisableSyntaxTreesitter()
+      echo("Big file, disabling syntax, treesitter and folding")
+      if exists(':TSBufDisable')
+          exec 'TSBufDisable autotag'
+          exec 'TSBufDisable highlight'
+          " etc...
+      endif
+
+      set foldmethod=manual
+      syntax clear
+      syntax off
+      filetype off
+      set noundofile
+      set noswapfile
+      set noloadplugins
+  endfunction
+
+  augroup BigFileDisable
+      autocmd!
+      autocmd BufReadPost * if getfsize(expand("%")) > 5 * 1024 * 1024 | exec DisableSyntaxTreesitter() | endif
+  augroup END
 ]])
 

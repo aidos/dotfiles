ef35a498e85d3935e1bb4a562aa8530045ed7eb1
Aidan Kane <aidan@countfire.com>
2022-01-29 23:24:34 +0000

    update for more recent ipython

--- _config/ipython/profile_default/ipython_config.py
+++ _config/ipython/profile_default/ipython_config.py
@@ -8,21 +8,10 @@ c.TerminalInteractiveShell.autoindent = False
 
 # c.TerminalInteractiveShell.editing_mode = 'vi'
 
-c.InteractiveShell.colors='linux'
+c.TerminalInteractiveShell.colors = 'linux'
+c.TerminalInteractiveShell.highlighting_style = 'dracula'
+c.TerminalInteractiveShell.highlight_matching_brackets = True
+
 
 #c.Completer.greedy = True
-
-
-## https://github.com/ipython/ipython/issues/9915
-# from IPython.terminal.prompts import Prompts, Token
-# from prompt_toolkit.key_binding.vi_state import InputMode
-# class MyPrompts(Prompts):
-#     def in_prompt_tokens(self, cli=None):
-#         mode = 'ins' if cli.vi_state.input_mode == InputMode.INSERT else 'cmd'
-#         return [
-#             (Token.Prompt, '(%s)In [' % mode),
-#             (Token.PromptNum, str(self.shell.execution_count)),
-#             (Token.Prompt, ']: ')
-#         ]
-# c.TerminalInteractiveShell.prompts_class = MyPrompts
 

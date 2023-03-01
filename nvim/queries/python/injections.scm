dee13c527287ec1492cae6d13062cd7e4dbfcb8f
Aidan Kane <aidan@countfire.com>
2023-03-01 12:31:31 +0000

    hacks

--- nvim/queries/python/injections.scm
+++ nvim/queries/python/injections.scm
@@ -6,3 +6,9 @@
 (#eq? @_func "text")
 (#offset! @sql 0 1 0 -1))
 
+((call
+  function: (identifier) @_func
+  arguments: (argument_list (string) @html))
+(#eq? @_func "HTMLString")
+(#offset! @html 0 1 0 -1))
+

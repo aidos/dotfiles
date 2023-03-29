d9d0df6296e6f416d477adc21be92ba32dd18d94
Aidan Kane <aidan@countfire.com>
2023-03-29 09:01:02 +0100

    remove dead conf

--- link.sh
+++ link.sh
@@ -4,7 +4,6 @@ ln -s ~/.dotfiles/_config/ipython ~/.ipython
 mkdir -p ~/.config
 mkdir -p ~/.ssh && ln -s ~/.dotfiles/_ssh/rc ~/.ssh/rc
 
-ln -s ~/.dotfiles/_ackrc ~/.ackrc
 cp ~/.dotfiles/_gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/_inputrc ~/.inputrc
 ln -s ~/.dotfiles/_tmux.conf ~/.tmux.conf

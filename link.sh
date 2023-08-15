e4db8baded930e424f1ce2d565bfbc737eed35f9
Aidan Kane <aidan@countfire.com>
2023-08-15 21:55:46 +0000

    Place for sharing custom scripts across environment

--- link.sh
+++ link.sh
@@ -7,3 +7,5 @@ mkdir -p ~/.ssh && ln -s ~/.dotfiles/_ssh/rc ~/.ssh/rc
 cp ~/.dotfiles/_gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/_inputrc ~/.inputrc
 ln -s ~/.dotfiles/_tmux.conf ~/.tmux.conf
+
+ln -s ~/.dotfiles/bin ~/bin

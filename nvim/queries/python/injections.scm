012229a9e458cf49b84d6b1341956d20cd0340d7
Aidan Kane <aidan@countfire.com>
2023-09-21 11:02:43 +0000

    Injections for python to match new syntax

--- nvim/queries/python/injections.scm
+++ nvim/queries/python/injections.scm
@@ -1,14 +1,22 @@
 ; inherits: python
 
-((call
-  function: (identifier) @_func
-  arguments: (argument_list (string) @sql))
-(#eq? @_func "text")
-(#offset! @sql 0 1 0 -1))
+(
+  (call
+    function: (identifier) @_func
+    arguments: (argument_list (string (string_content) @injection.content))
+  )
+  (#eq? @_func "text")
+  (#set! injection.language "sql")
+  (#set! injection.include-children)
+)
 
-((call
-  function: (identifier) @_func
-  arguments: (argument_list (string) @html))
-(#eq? @_func "HTMLString")
-(#offset! @html 0 1 0 -1))
+(
+  (call
+    function: (identifier) @_func
+    arguments: (argument_list (string (string_content) @injection.content))
+  )
+  (#eq? @_func "HTMLString")
+  (#set! injection.language "html")
+  (#set! injection.include-children)
+)
 

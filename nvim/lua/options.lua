470feda469c751d03e2b9a572c9b10c8b485430c
Aidan Kane <aidan@countfire.com>
2024-01-27 00:56:25 +0000

    Dulling down pyright a little

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -464,14 +464,44 @@ augroup END
 ]])
 
 vim.diagnostic.config({
-    float = {
+  float = {
     source = 'always',
     border = border
   },
 })
 
+
+function filter(arr, func)
+  local new_index = 1
+  local size_orig = #arr
+  for old_index, v in ipairs(arr) do
+    if func(v, old_index) then
+      arr[new_index] = v
+      new_index = new_index + 1
+    end
+  end
+  for i = new_index, size_orig do arr[i] = nil end
+end
+function filter_diagnostics(diagnostic)
+  -- Only filter out Pyright stuff for now
+  if diagnostic.source ~= "Pyright" then
+    return true
+  end
+  if diagnostic.message == '"kwargs" is not accessed' then
+    return false
+  end
+  -- Handled by Ruff
+  if string.match(diagnostic.message, '.+ is not accessed') then
+    return false
+  end
+  return true
+end
+function custom_on_publish_diagnostics(a, params, client_id, c, config)
+  filter(params.diagnostics, filter_diagnostics)
+  vim.lsp.diagnostic.on_publish_diagnostics(a, params, client_id, c, config)
+end
 lsp.handlers['textDocument/publishDiagnostics'] = lsp.with(
-  lsp.diagnostic.on_publish_diagnostics,
+  custom_on_publish_diagnostics,
   {
     underline = true,
     virtual_text = {
@@ -481,6 +511,7 @@ lsp.handlers['textDocument/publishDiagnostics'] = lsp.with(
     update_in_insert = false,
   }
 )
+
 
 -- stops windows shuffling about
 require("stabilize").setup()
